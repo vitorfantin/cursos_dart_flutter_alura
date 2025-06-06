3e5ae097af430bc5c6feb0a7774e117a6afb7db2
Vitor Fantin <fantinfx@gmail.com>
2025-06-06 20:05:59 -0300

    criando a logica da calculadora usando if else  sem tratamento de erro

--- calculadora.dart
+++ calculadora.dart
@@ -1,24 +1,37 @@
 import 'dart:io';
 
 void main() {
- print("*--* Bem-vindo(a) ao Calculadora Fantin ! *--*");
+  double resultado = 0;
+  print("--------------------");
 
- print("************");
+  print("*--* Bem-vindo(a) ao Calculadora Fantin ! *--*");
 
-print("Digite um número: ");
-double n1 = double.parse(stdin.readLineSync()!);
+  print("--------------------");
 
- print("************");
+  print("Digite um número: ");
+  double n1 = double.parse(stdin.readLineSync()!);
 
-print("Digite qual operação deseja realizar (+ | - | * | / : )");
-String? operacao = stdin.readLineSync();
+  print("--------------------");
 
- print("************");
+  print("Digite qual operação deseja realizar  + | - | * | / ");
+  String? operacao = stdin.readLineSync();
 
-print("digite o segundo número: ");
-double n2 = double.parse(stdin.readLineSync()!);
+  print("--------------------");
 
-print ("$n1, $operacao, $n2 ");
+  print("digite o segundo número: ");
+  double n2 = double.parse(stdin.readLineSync()!);
 
-
+  if (operacao == "+") {
+    resultado = n1 + n2;
+    print(" O resultado de $n1 + $n2 => $resultado ");
+  } else if (operacao == "-") {
+    resultado = n1 - n2;
+    print(" O resultado de $n1 - $n2 => $resultado ");
+  } else if (operacao == "*") {
+    resultado = n1 * n2;
+    print(" O resultado de $n1 * $n2 => $resultado ");
+  } else {
+    resultado = n1 / n2;
+    print(" O resultado de $n1 / $n2 => $resultado ");
+  }
 }
